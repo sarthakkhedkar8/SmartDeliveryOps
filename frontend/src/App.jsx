5ca120d48ae277b99e7b17ac29ba5e72852b7f80
Sarthak <sarthakkhedkar4@gmail.com>
2026-09-07 10:49:36 +0530

    Day 10: Deploy SmartDeliveryOps on Kubernetes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://localhost:8001";
+const API_URL = "/api";
 
 function App() {
   const [deliveries, setDeliveries] = useState([]);
